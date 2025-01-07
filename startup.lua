ffb9ab7d51afc6d156a46568f970e2235d1f4979
banana-boye <bferraz1313@gmail.com>
2025-01-07 23:36:31 +0100

    rarg

--- startup.lua
+++ startup.lua
@@ -5,11 +5,11 @@ if https.get("https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads
     for _, file in pairs(fs.list("./")) do
         shell.run("delete "..file)
     end
-    shell.run("wget get https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/startup.lua startup")
+    shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/startup.lua startup")
     shell.run("reboot")
 end
-if not fs.exist("basalt.lua") then shell.run("wget get https://basalt.madefor.cc/install.lua release basalt.lua") end
-if not fs.exist("main.lua") then shell.run("wget get https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/main.lua") end
+if not fs.exist("basalt.lua") then shell.run("wget https://basalt.madefor.cc/install.lua release basalt.lua") end
+if not fs.exist("main.lua") then shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/main.lua") end
 
 -- Run
 shell.run("main.lua")
