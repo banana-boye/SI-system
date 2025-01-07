a5d82c5e0aa849cf539ef88a4eefc796c1ded698
banana-boye <bferraz1313@gmail.com>
2025-01-07 23:34:58 +0100

    test

--- startup.lua
+++ startup.lua
@@ -1,6 +1,13 @@
 -- Check if all files exist
+local version = "0"
 
-if https.get() then end
+if https.get("https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/version.txt").readAll() ~= version then
+    for _, file in pairs(fs.list("./")) do
+        shell.run("delete "..file)
+    end
+    shell.run("wget get https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/startup.lua startup")
+    shell.run("reboot")
+end
 if not fs.exist("basalt.lua") then shell.run("wget get https://basalt.madefor.cc/install.lua release basalt.lua") end
 if not fs.exist("main.lua") then shell.run("wget get https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/main.lua") end
 
