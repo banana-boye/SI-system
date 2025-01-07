329f4ff4a1b14e45d8505a921a9b5ee1d40d87d9
banana-boye <bferraz1313@gmail.com>
2025-01-07 23:29:23 +0100

    ver

--- startup.lua
+++ startup.lua
@@ -1,7 +1,8 @@
 -- Check if all files exist
 
-if not fs.exist("basalt.lua") then shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua") end
-if not fs.exist("main.lua") then shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua") end
+if https.get() then end
+if not fs.exist("basalt.lua") then shell.run("wget get https://basalt.madefor.cc/install.lua release basalt.lua") end
+if not fs.exist("main.lua") then shell.run("wget get https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/main.lua") end
 
 -- Run
 shell.run("main.lua")
