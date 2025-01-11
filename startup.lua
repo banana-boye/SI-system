5ae6e0b4ead15fa8ad9cf85b94e878e3cb7c2d61
banana-boye <bferraz1313@gmail.com>
2025-01-11 08:36:57 +0100

    rargh

--- startup.lua
+++ startup.lua
@@ -9,16 +9,16 @@ if http.get("https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/
         end
     end
     shell.run("delete startup.lua")
-    shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/startup.lua startup")
+    shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/startup.lua startup.lua")
     shell.run("reboot")
 end
 if not fs.exists("SI/basalt.lua") then shell.run("wget run https://basalt.madefor.cc/install.lua release latest.lua SI/basalt.lua") end
 if not fs.exists("SI/main.lua") then shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/main.lua SI/main.lua") end
-if not fs.exists("SI/extratools.lua") then shell.run("wget https://github.com/banana-boye/SI-system/blob/main/SI/extratools.lua SI/extratools.lua") end
-if not fs.exists("SI/synth.lua") then shell.run("wget https://github.com/banana-boye/SI-system/blob/main/SI/synth.lua SI/synth.lua") end
-if not fs.exists("SI/withdraw.lua") then shell.run("wget https://github.com/banana-boye/SI-system/blob/main/SI/withdraw.lua SI/withdraw.lua") end
-if not fs.exists("SI/deposit.lua") then shell.run("wget https://github.com/banana-boye/SI-system/blob/main/SI/deposit.lua SI/deposit.lua") end
-if not fs.exists("SI/organize.lua") then shell.run("wget https://github.com/banana-boye/SI-system/blob/main/SI/organize.lua SI/organize.lua") end
+if not fs.exists("SI/extratools.lua") then shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/SI/extratools.lua SI/extratools.lua") end
+if not fs.exists("SI/synth.lua") then shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/SI/synth.lua SI/synth.lua") end
+if not fs.exists("SI/withdraw.lua") then shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/SI/withdraw.lua SI/withdraw.lua") end
+if not fs.exists("SI/deposit.lua") then shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/SI/deposit.lua SI/deposit.lua") end
+if not fs.exists("SI/organize.lua") then shell.run("wget https://raw.githubusercontent.com/banana-boye/SI-system/refs/heads/main/SI/organize.lua SI/organize.lua") end
 
 -- Run
 shell.run("SI/main.lua")
